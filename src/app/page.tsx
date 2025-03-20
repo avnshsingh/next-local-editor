3448cf5ca024264c4f35bca63370e9f8677affb6
Avinash Singh <avinashsingh888555@gmail.com>
2025-03-20 08:42:58 +0530

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Video from "@/component/Video";
 import NoSSRWrapper from "./NoSSRWrapper";
 import Transcript from "@/component/Transcript";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
