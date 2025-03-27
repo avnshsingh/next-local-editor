832edf1ae77596a3cecd5d229a052c2a6a298b70
Avinash Singh <avinashsingh888555@gmail.com>
2025-03-27 10:29:42 +0530

    disable any ts check while building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,6 @@
 
 import Video from "@/comp-pages/Video";
 import NoSSRWrapper from "./NoSSRWrapper";
-import Transcript from "@/comp-pages/Transcript";
-import { Button } from "@/components/ui/button";
-
-import ModelManager from "@/comp-pages/ModelManager";
-import TranscriberSTT from "@/comp-pages/TranscriberSTT";
 
 export default function Page() {
   return (
